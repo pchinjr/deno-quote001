7c33a02563c19dd48107f2981c7b15450725f531
Paul Chin Jr <pchinjr@gmail.com>
2020-05-14 00:54:27 -0400

    content type

--- src/http/get-api/index.ts
+++ src/http/get-api/index.ts
@@ -5,7 +5,7 @@ export async function handler (req: object) {
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
+      'content-type': 'application/json; charset=utf8'
     },
     body: JSON.stringify( {data: quote() } )
   }
